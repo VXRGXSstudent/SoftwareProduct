5ce1bcbf297e6fd90bcaf045328099a4ef436f15
VXRGXSstudent <joao.vargas@aluno.faculdadeimpacta.com.br>
2023-10-16 23:58:03 -0300

    SecondCommit

--- conversao.py
+++ conversao.py
@@ -3,7 +3,19 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/adicionar_carrinho', methods=['GET', 'POST'])
+def adicionar_carrinho():
+    qtd = None
+    preco = None
+    if request.method == 'POST':
+        preco = float(request.form['preco'])
+        qtd = int(request.form.get('quantidade'))
+
+    return render_template('home.html', preco=preco, qtd=qtd)
+
+
+@app.route('/conversao', methods=['GET', 'POST'])
 def index():
     valor_convertido = None
     moeda = None
@@ -14,10 +26,6 @@ def index():
         valor_convertido, moeda = converter_para_real(valor, moeda_origem)
 
     return render_template('site.html', valor_convertido=valor_convertido, moeda=moeda)
-
-@app.route('/home', endpoint='home')
-def loja():
-    return render_template('home.html')
 
 link = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
 link_dic = link.json()
@@ -46,6 +54,8 @@ def converter_para_real(valor, moeda_origem):
         return None, None
     valor_convertido = valor / cotacao
     return valor_convertido, moeda
+
+
 
 if __name__ == '__main__':
     app.run()
