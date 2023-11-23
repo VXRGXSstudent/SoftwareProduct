c034ffe442356656667dcb7e8db619b7aa1bb0c9
VXRGXSstudent <joao.vargas@aluno.faculdadeimpacta.com.br>
2023-11-23 18:53:58 -0300

    Last

--- conversao.py
+++ conversao.py
@@ -1,24 +1,76 @@
 import requests
-from flask import Flask, render_template, request, url_for, jsonify
+from flask import Flask, render_template, request, jsonify
 
+# Inicialização do aplicativo Flask
 app = Flask(__name__, template_folder='templates')
 
+# Api armarzenada em uma variável
+link = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+link_dic = link.json()
 
-@app.route('/adicionar_carrinho', methods=['GET', 'POST'])
+# Rota para a página inicial
+@app.route('/index')
+def index():
+    return render_template('home.html')
+
+# Lista para armazenar itens do carrinho
+carrinho = []
+
+# Rota para adicionar itens ao carrinho via requisição POST
+@app.route('/adicionar_carrinho', methods=['POST'])
 def adicionar_carrinho():
-    qtd = None
-    preco_em_reais = None
-    moeda = None
-    dolar_real = cotacao_dolar_real()
-    euro_real = cotacao_euro_real()
-    bitcoin_real = cotacao_bitcoin_real()
-    if request.method == 'POST':
-        moeda = request.form['moeda']
-        preco_em_reais = float(request.form['preco_reais'])
-        qtd = int(request.form.get('quantidade'))
+    try:
+        data = request.get_json()
 
-    return render_template('home.html', preco_em_reais=preco_em_reais, qtd=qtd, moeda=moeda, dolar_real=dolar_real, euro_real=euro_real, bitcoin_real=bitcoin_real)
+        moeda = data.get('moeda')
+        preco_reais = float(data.get('preco_reais'))
+        quantidade = int(data.get('quantidade'))
 
+        # Obter as taxas de câmbio uma vez para evitar inconsistências
+        taxas_cambio = {
+            'USD': cotacao_dolar_real(),
+            'EUR': cotacao_euro_real(),
+            'BTC': cotacao_bitcoin_real()
+        }
+
+        # Usar a função correspondente à moeda selecionada
+        if moeda not in taxas_cambio:
+            raise ValueError("Moeda não suportada")
+
+        taxa_cambio = taxas_cambio[moeda]
+
+        # Calcular o preço convertido
+        preco_convertido = preco_reais / taxa_cambio
+
+        # Adicionar item ao carrinho
+        adicionar_item_carrinho(moeda, preco_convertido, quantidade)
+
+        # Retorna as informações necessárias para o JavaScript
+        return jsonify({
+            'moeda': moeda,
+            'preco_convertido': preco_convertido,
+            'quantidade': quantidade,
+            'taxas_cambio': taxas_cambio
+        })
+
+    except Exception as e:
+        # Em caso de erro, retornar uma resposta com status 400 e uma mensagem de erro
+        return jsonify({'error': str(e)}), 400
+
+# Função para adicionar item ao carrinho
+def adicionar_item_carrinho(moeda, preco_reais, quantidade):
+    # Lógica para adicionar o item ao carrinho (pode ser armazenado em variáveis, banco de dados, etc.)
+
+    # Exemplo simples: armazenar em um dicionário
+    item_carrinho = {
+        'moeda': moeda,
+        'preco_reais': preco_reais,
+        'quantidade': quantidade
+    }
+
+    carrinho.append(item_carrinho)
+
+# Rota para obter taxas de câmbio
 @app.route('/taxas_cambio', methods=['GET'])
 def taxas_cambio():
     taxas = {
@@ -28,48 +80,18 @@ def taxas_cambio():
     }
     return jsonify(taxas)
 
-@app.route('/conversao', methods=['GET', 'POST'])
-def index():
-    valor_convertido = None
-    moeda = None
-
-    if request.method == 'POST':
-        valor = float(request.form['valor'])
-        moeda_origem = request.form['moeda_origem'].upper()
-        valor_convertido, moeda = converter_para_real(valor, moeda_origem)
-
-    return render_template('site.html', valor_convertido=valor_convertido, moeda=moeda)
-
-link = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
-link_dic = link.json()
-
+# Função para obter a cotação do dólar em relação ao real
 def cotacao_dolar_real():
     return float(link_dic['USDBRL']['bid'])
 
+# Função para obter a cotação do euro em relação ao real
 def cotacao_euro_real():
     return float(link_dic['EURBRL']['bid'])
 
+# Função para obter a cotação do bitcoin em relação ao real
 def cotacao_bitcoin_real():
     return float(link_dic['BTCBRL']['bid'])
 
-def converter_para_real(valor, moeda_origem):
-    if moeda_origem == 'USD':
-        cotacao = cotacao_dolar_real()
-        moeda = 'USD'
-    elif moeda_origem == 'EUR':
-        cotacao = cotacao_euro_real()
-        moeda = 'EUR'
-    elif moeda_origem == 'BTC':
-        cotacao = cotacao_bitcoin_real()
-        moeda = 'BTC'
-    else:
-        print("Moeda de origem não suportada.")
-        return None, None
-    valor_convertido = valor / cotacao
-    return valor_convertido, moeda
-
-
-
+# Executa o aplicativo Flask
 if __name__ == '__main__':
     app.run()
- 
--- static/script.js
+++ static/script.js
@@ -1,42 +1,168 @@
+// Função para converter o preço original para a moeda selecionada com base nas taxas de câmbio
+function converterMoeda(precoOriginal, moedaSelecionada, taxasCambio) {
+    const taxaCambio = taxasCambio[moedaSelecionada];
+
+    if (moedaSelecionada === "BRL") {
+        return precoOriginal;
+    } else {
+        return precoOriginal / taxaCambio;
+    }
+}
+
+// Função para abrir/fechar o carrinho e aplicar efeitos visuais
+function abrirCarrinho(abrir) {
+    const carrinhoSidebar = document.getElementById('carrinho-sidebar');
+    carrinhoSidebar.style.display = abrir ? 'block' : 'none';
+    desfocarFundo(abrir);
+}
+
+// Função para aplicar o efeito de desfoque no fundo ao abrir o carrinho
+function desfocarFundo(destacar) {
+    const principal = document.querySelector('.principal');
+    principal.style.filter = destacar ? 'blur(5px)' : 'none';
+}
+
+// Função para atualizar dinamicamente o conteúdo do carrinho na barra lateral
+function atualizarCarrinho(taxasCambio) {
+    const carrinhoContainer = document.getElementById('carrinho-sidebar');
+    const listaCarrinho = document.getElementById('carrinho-lista');
+
+    listaCarrinho.innerHTML = '';
+    let total = 0;
+
+    carrinho.forEach((item) => {
+        const itemLista = document.createElement('li');
+
+        // Verifica se a taxa de câmbio para a moeda selecionada é diferente de zero
+        if (taxasCambio[item.moeda] !== 0) {
+            // Converte o preço para a moeda selecionada
+            const precoConvertido = converterMoeda(item.precoReais, item.moeda, taxasCambio);
+
+            // Exibe o item no formato desejado
+            itemLista.textContent = `${item.quantidade} x ${precoConvertido.toFixed(2)} ${item.moeda}`;
+            listaCarrinho.appendChild(itemLista);
+
+            // Adiciona ao total convertendo para a moeda selecionada
+            total += parseFloat(precoConvertido) * item.quantidade;
+        } else {
+            // Se a taxa de câmbio for zero, o preço não é convertido
+            itemLista.textContent = `${item.quantidade} x ${item.precoReais} BRL`;
+            listaCarrinho.appendChild(itemLista);
+        }
+    });
+
+    const moedaSelecionada = document.getElementById("selectmoeda").value;
+    const valorTotal = document.getElementById('valor-total');
+
+    // Exibe o total convertido para a moeda selecionada
+    valorTotal.textContent = `${total.toFixed(2)} ${moedaSelecionada}`;
+
+    // Exibir o container do carrinho apenas se houver itens no carrinho
+    carrinhoContainer.style.display = carrinho.length > 0 ? 'block' : 'none';
+}
+
+// Função assíncrona para adicionar um item ao carrinho via requisição POST
+async function adicionarAoCarrinho(event, produtoId) {
+    event.preventDefault();
+
+    const moedaSelecionada = document.getElementById('selectmoeda').value;
+    const quantidade = parseInt(document.getElementById(`quantidade${produtoId}`).value);
+    const precoOriginal = parseFloat(document.querySelector(`.produto[data-produto-id="${produtoId}"]`).getAttribute('data-preco-original'));
+
+    try {
+        // Obtém as taxas de câmbio
+        const response = await fetch('/taxas_cambio');
+        const taxasCambio = await response.json();
+
+        // Converte o preço para a moeda selecionada
+        const precoConvertido = converterMoeda(precoOriginal, moedaSelecionada, taxasCambio);
+
+        // Adiciona o item ao carrinho
+        if (!isNaN(precoConvertido)) {
+            carrinho.push({
+                moeda: moedaSelecionada,
+                precoReais: precoOriginal,  // Use o preço original aqui
+                precoConvertido: precoConvertido,
+                quantidade: quantidade
+            });
+
+            // Atualiza a exibição do carrinho
+            atualizarCarrinho(taxasCambio);
+            desfocarFundo(true);
+        } else {
+            alert('A taxa de câmbio para a moeda selecionada é zero. O item não foi adicionado ao carrinho.');
+        }
+    } catch (error) {
+        console.error('Erro ao obter taxas de câmbio:', error);
+    }
+}
+
+// Função para limpar o carrinho
+function limparCarrinho() {
+    carrinho.length = 0; // Limpa a lista de itens do carrinho
+    atualizarCarrinho(taxasCambio); // Atualiza a exibição do carrinho
+    desfocarFundo(false)
+}
+
+// Array para armazenar itens do carrinho
+const carrinho = [];
+
+// Objeto para armazenar as taxas de câmbio
+let taxasCambio = null;
+
+// Função executada quando o DOM está totalmente carregado
 document.addEventListener('DOMContentLoaded', function () {
+    // Função para atualizar os preços dos produtos na moeda selecionada
     function atualizarPrecos() {
         const moedaSelecionada = document.getElementById("selectmoeda").value;
         const produtos = document.querySelectorAll(".produto");
 
-        // Faça uma solicitação AJAX para obter as taxas de câmbio
-        fetch('/taxas_cambio')
-            .then(response => response.json())
-            .then(data => {
-                produtos.forEach(function (produto) {
-                    const precoOriginal = parseFloat(produto.getAttribute('data-preco-original'));
+        fetch('/taxas_cambio').then(response => response.json()).then(data => {
+            taxasCambio = data;
 
-                    if (!isNaN(precoOriginal)) {
-                        const novoPreco = converterMoeda(precoOriginal, moedaSelecionada, data);
-                        produto.querySelector("h3").textContent = `${produto.querySelector("h3").textContent.split('-')[0]} - ${novoPreco} ${moedaSelecionada}`;
+            produtos.forEach(function (produto) {
+                const precoOriginal = parseFloat(produto.getAttribute("data-preco-original"));
+
+                if (precoOriginal !== 0) {
+                    const taxaCambio = taxasCambio[moedaSelecionada];
+
+                    if (taxaCambio !== 0) {
+                        const precoConvertido = converterMoeda(precoOriginal, moedaSelecionada, taxasCambio);
+                        produto.querySelector("h3").textContent = `${produto.querySelector("h3").textContent.split("-")[0]} - ${precoConvertido.toFixed(2)} ${moedaSelecionada}`;
+                    } else {
+                        produto.querySelector("h3").textContent = `${produto.querySelector("h3").textContent.split("-")[0]} - ${precoOriginal} BRL`;
                     }
-                });
+                }
             });
+        });
     }
 
-    // A função de conversão de moeda aqui...
-    function converterMoeda(precoOriginal, moeda, taxasCambio) {
-        switch (moeda) {
-            case 'USD':
-                return (precoOriginal / taxasCambio.USD).toFixed(2);
-            case 'EUR':
-                return (precoOriginal / taxasCambio.EUR).toFixed(2);
-            case 'BTC':
-                return (precoOriginal / taxasCambio.BTC).toFixed(8);
-            default:
-                return precoOriginal.toFixed(2);
-        }
-    }
-
-    // Essa parte garante que os preços sejam atualizados quando a página for carregada
-    window.addEventListener("load", atualizarPrecos);
-
-    // Manipulador de eventos para o elemento selectmoeda
+    // Event listener para atualizar os preços ao alterar a moeda no seletor
     document.getElementById("selectmoeda").addEventListener("change", function () {
         atualizarPrecos();
     });
+
+    // Event listener para adicionar itens ao carrinho ao enviar o formulário
+    const produtos = document.querySelectorAll('.produto form');
+
+    produtos.forEach((produtoForm) => {
+        produtoForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            const produtoId = parseInt(this.closest('.produto').getAttribute('data-produto-id'));
+            adicionarAoCarrinho(event, produtoId);
+        });
+    });
+
+    // Adiciona um event listener para fechar o carrinho ao clicar no fundo desfocado
+    const principal = document.querySelector('.principal');
+    principal.addEventListener('click', function (event) {
+        const carrinhoSidebar = document.getElementById('carrinho-sidebar');
+        if (carrinhoSidebar.style.display === 'block') {
+            abrirCarrinho(false); // Feche o carrinho se estiver aberto
+        }
+    });
+
+     // Adiciona um event listener para o botão "Clear Cart"
+     document.getElementById('limpar-carrinho').addEventListener('click', limparCarrinho);
 });
